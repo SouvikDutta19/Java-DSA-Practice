832deaf046cae4b5bcd3a6f685823e2237b1f446
Souvik_Dutta <souvikdutta491@gmail.com>
2025-07-03 09:17:11 +0530

    Day 3: Print all prime numbers up to a given limit

---
+++ Day3_PrintPrimeNumbers.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Day3_PrintPrimeNumbers {
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+
+    public static void printPrimes(int limit) {
+        System.out.println("🔢 Prime numbers up to " + limit + ":");
+        for (int i = 2; i <= limit; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter limit: ");
+        int n = sc.nextInt();
+        printPrimes(n);
+        sc.close();
+    }
+}
