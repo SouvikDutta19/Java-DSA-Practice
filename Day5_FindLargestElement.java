d2339e403b6c8e864101d552cedd85ccb782dd31
Souvik_Dutta <souvikdutta491@gmail.com>
2025-07-05 20:05:14 +0530

    Day 5: Find the largest element in an array

---
+++ Day5_FindLargestElement.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Day5_FindLargestElement {
+
+    public static int findMax(int[] arr) {
+        int max = arr[0];
+        for (int num : arr) {
+            if (num > max) max = num;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int largest = findMax(arr);
+        System.out.println("🔝 Largest element: " + largest);
+
+        sc.close();
+    }
+}
