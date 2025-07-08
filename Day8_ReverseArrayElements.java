c6676bfc9f9e2f46f7c9f3df18bf84b357b63bca
Souvik_Dutta <souvikdutta491@gmail.com>
2025-07-08 10:03:17 +0530

    Day 8: Reverse the elements of an array in-place

---
+++ Day8_ReverseArrayElements.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Day8_ReverseArrayElements {
+
+    public static void reverseArray(int[] arr) {
+        int start = 0, end = arr.length - 1;
+        while (start < end) {
+            int temp = arr[start];
+            arr[start] = arr[end];
+            arr[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+        reverseArray(arr);
+        System.out.println("🔄 Reversed array: " + Arrays.toString(arr));
+
+        sc.close();
+    }
+}
