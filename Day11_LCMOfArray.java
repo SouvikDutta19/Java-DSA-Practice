574f1b80f019e2e68034269f509efa83f559b06a
Souvik_Dutta <souvikdutta491@gmail.com>
2025-07-11 08:43:03 +0530

    Day 11: Find LCM of all elements in an array

---
+++ Day11_LCMOfArray.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Day11_LCMOfArray {
+
+    public static int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
+
+    public static int lcm(int a, int b) {
+        return (a * b) / gcd(a, b);
+    }
+
+    public static int lcmArray(int[] arr) {
+        int result = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            result = lcm(result, arr[i]);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+        System.out.println("📐 LCM of array: " + lcmArray(arr));
+        sc.close();
+    }
+}
