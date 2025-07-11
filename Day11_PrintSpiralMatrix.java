aaca1249d781c0c0c10229a957f84751295a9156
Souvik_Dutta <souvikdutta491@gmail.com>
2025-07-11 08:48:02 +0530

    Day 11: Print matrix elements in spiral order

---
+++ Day11_PrintSpiralMatrix.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Day11_PrintSpiralMatrix {
+
+    public static void spiralPrint(int[][] matrix, int r, int c) {
+        int top = 0, bottom = r - 1;
+        int left = 0, right = c - 1;
+
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++)
+                System.out.print(matrix[top][i] + " ");
+            top++;
+
+            for (int i = top; i <= bottom; i++)
+                System.out.print(matrix[i][right] + " ");
+            right--;
+
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--)
+                    System.out.print(matrix[bottom][i] + " ");
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--)
+                    System.out.print(matrix[i][left] + " ");
+                left++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter rows and columns: ");
+        int r = sc.nextInt(), c = sc.nextInt();
+        int[][] matrix = new int[r][c];
+
+        System.out.println("Enter matrix:");
+        for (int i = 0; i < r; i++)
+            for (int j = 0; j < c; j++)
+                matrix[i][j] = sc.nextInt();
+
+        System.out.println("🌀 Spiral order:");
+        spiralPrint(matrix, r, c);
+        sc.close();
+    }
+}
