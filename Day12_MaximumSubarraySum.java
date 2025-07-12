99fffe91a8862bce45d544410cfbcdda034b6435
Souvik_Dutta <souvikdutta491@gmail.com>
2025-07-12 20:05:52 +0530

    Day 12: Find the maximum sum of a subarray (Kadane’s algorithm)

---
+++ Day12_MaximumSubarraySum.java
@@ -0,0 +1,16 @@
+public class Day12_MaximumSubarraySum {
+
+    public static int maxSubArray(int[] nums) {
+        int maxSoFar = nums[0], currentMax = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            currentMax = Math.max(nums[i], currentMax + nums[i]);
+            maxSoFar = Math.max(maxSoFar, currentMax);
+        }
+        return maxSoFar;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println("📈 Maximum Subarray Sum: " + maxSubArray(arr));
+    }
+}
