816804e9e86ab089a71432ca441da136df6fdbcd
Souvik Dutta <116168757+SouvikDutta19@users.noreply.github.com>
2025-07-19 20:48:07 +0530

    Day19: Created MinStack with constant time min retrieval

---
+++ day19_MinStack.java
@@ -0,0 +1,47 @@
+import java.util.Stack;
+
+public class MinStack {
+    private Stack<Integer> stack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        if (stack.pop().equals(minStack.peek())) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+
+    public static void main(String[] args) {
+        MinStack ms = new MinStack();
+        ms.push(2);
+        ms.push(0);
+        ms.push(3);
+        ms.push(0);
+        System.out.println(ms.getMin()); // 0
+        ms.pop();
+        System.out.println(ms.getMin()); // 0
+        ms.pop();
+        System.out.println(ms.getMin()); // 0
+        ms.pop();
+        System.out.println(ms.getMin()); // 2
+    }
+}
