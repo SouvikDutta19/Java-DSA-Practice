b0b1f5127b21ffba8d9534a4abaa7cee3a33c696
Souvik_Dutta <souvikdutta491@gmail.com>
2025-07-27 19:57:07 +0530

    Add Max Product Subarray using dynamic programming approach

---
+++ MaxProductSubarray.java
@@ -0,0 +1,19 @@
+public class MaxProductSubarray {
+    public static int maxProduct(int[] nums) {
+        int max = nums[0], min = nums[0], result = nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            int temp = max;
+            max = Math.max(nums[i], Math.max(max * nums[i], min * nums[i]));
+            min = Math.min(nums[i], Math.min(temp * nums[i], min * nums[i]));
+            result = Math.max(result, max);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 3, -2, 4};
+        System.out.println("Maximum product subarray: " + maxProduct(nums));
+    }
+}
