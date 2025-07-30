d7a44e23072d8b6e82bd9089d48b7a8a53247046
Souvik_Dutta <souvikdutta491@gmail.com>
2025-07-30 22:10:06 +0530

    Implemented Sliding Window Maximum using Deque - day30_slidingwindowmaximum.java

---
+++ day30_slidingwindowmaximum.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class day30_slidingwindowmaximum {
+    public static int[] maxSlidingWindow(int[] nums, int k) {
+        Deque<Integer> dq = new ArrayDeque<>();
+        int[] res = new int[nums.length - k + 1];
+        int idx = 0;
+        for (int i = 0; i < nums.length; i++) {
+            while (!dq.isEmpty() && dq.peekFirst() <= i - k) dq.pollFirst();
+            while (!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) dq.pollLast();
+            dq.offerLast(i);
+            if (i >= k - 1) res[idx++] = nums[dq.peekFirst()];
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,3,-1,-3,5,3,6,7};
+        int[] res = maxSlidingWindow(nums, 3);
+        System.out.println(Arrays.toString(res)); // [3,3,5,5,6,7]
+    }
+}
