66aab6c5af8330200148a25d2870cee46e2c764c
Souvik_Dutta <souvikdutta491@gmail.com>
2025-08-03 22:53:59 +0530

    Add program to find kth largest element using min-heap

---
+++ day34_kthlargestelement.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class day34_kthlargestelement {
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int n : nums) {
+            pq.offer(n);
+            if (pq.size() > k) pq.poll();
+        }
+        return pq.peek();
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3,2,1,5,6,4};
+        int k = 2;
+        System.out.println("Kth largest element: " + findKthLargest(nums, k));
+    }
+}
