d30fea3097a8d3514e9115355b82bb66565c0e1f
Souvik_Dutta <souvikdutta491@gmail.com>
2025-08-04 12:24:16 +0530

    Add merge function to combine two sorted arrays in-place

---
+++ day35_mergetwosortedarrays.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+public class day35_mergetwosortedarrays {
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int p1 = m - 1, p2 = n - 1, p = m + n - 1;
+        while (p1 >= 0 && p2 >= 0) {
+            if (nums1[p1] > nums2[p2]) nums1[p--] = nums1[p1--];
+            else nums1[p--] = nums2[p2--];
+        }
+        while (p2 >= 0) nums1[p--] = nums2[p2--];
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {1,2,3,0,0,0};
+        int[] nums2 = {2,5,6};
+        merge(nums1, 3, nums2, 3);
+        System.out.println("Merged array: " + Arrays.toString(nums1));
+    }
+}
