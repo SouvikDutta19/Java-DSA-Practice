f0543fa75d351aa4940e1e1d290562d61ad0f24b
Souvik_Dutta <souvikdutta491@gmail.com>
2025-08-06 19:33:40 +0530

    Implement stack using two queues (Day 37)

---
+++ day37_implement_stack_using_queues.java
@@ -0,0 +1,43 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class day37_implement_stack_using_queues {
+    static class MyStack {
+        Queue<Integer> q1 = new LinkedList<>();
+        Queue<Integer> q2 = new LinkedList<>();
+
+        public void push(int x) {
+            q2.add(x);
+            while (!q1.isEmpty()) {
+                q2.add(q1.remove());
+            }
+            Queue<Integer> temp = q1;
+            q1 = q2;
+            q2 = temp;
+        }
+
+        public int pop() {
+            if (q1.isEmpty()) return -1;
+            return q1.remove();
+        }
+
+        public int top() {
+            if (q1.isEmpty()) return -1;
+            return q1.peek();
+        }
+
+        public boolean isEmpty() {
+            return q1.isEmpty();
+        }
+    }
+
+    public static void main(String[] args) {
+        MyStack stack = new MyStack();
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        System.out.println("Top: " + stack.top());
+        stack.pop();
+        System.out.println("Top after pop: " + stack.top());
+    }
+}
