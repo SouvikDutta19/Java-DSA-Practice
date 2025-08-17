03ee27af7cf39a22a73f4179ef7204dd1091e0e5
Souvik_Dutta <souvikdutta491@gmail.com>
2025-08-17 21:03:33 +0530

    Implemented Kruskal's algorithm for Minimum Spanning Tree in day48_minimum_spanning_tree.java

---
+++ day48_minimum_spanning_tree.java
@@ -0,0 +1,93 @@
+import java.util.*;
+
+class Edge implements Comparable<Edge> {
+    int src, dest, weight;
+
+    Edge(int s, int d, int w) {
+        src = s;
+        dest = d;
+        weight = w;
+    }
+
+    public int compareTo(Edge compareEdge) {
+        return this.weight - compareEdge.weight;
+    }
+}
+
+class Subset {
+    int parent, rank;
+}
+
+public class day48_minimum_spanning_tree {
+    int V, E;
+    Edge[] edges;
+
+    day48_minimum_spanning_tree(int v, int e) {
+        V = v;
+        E = e;
+        edges = new Edge[e];
+    }
+
+    int find(Subset[] subsets, int i) {
+        if (subsets[i].parent != i)
+            subsets[i].parent = find(subsets, subsets[i].parent);
+        return subsets[i].parent;
+    }
+
+    void union(Subset[] subsets, int x, int y) {
+        int xroot = find(subsets, x);
+        int yroot = find(subsets, y);
+
+        if (subsets[xroot].rank < subsets[yroot].rank)
+            subsets[xroot].parent = yroot;
+        else if (subsets[xroot].rank > subsets[yroot].rank)
+            subsets[yroot].parent = xroot;
+        else {
+            subsets[yroot].parent = xroot;
+            subsets[xroot].rank++;
+        }
+    }
+
+    void kruskalMST() {
+        Edge[] result = new Edge[V];
+        int e = 0;
+        int i = 0;
+        Arrays.sort(edges);
+
+        Subset[] subsets = new Subset[V];
+        for (int v = 0; v < V; ++v) {
+            subsets[v] = new Subset();
+            subsets[v].parent = v;
+            subsets[v].rank = 0;
+        }
+
+        while (e < V - 1 && i < E) {
+            Edge next_edge = edges[i++];
+            int x = find(subsets, next_edge.src);
+            int y = find(subsets, next_edge.dest);
+
+            if (x != y) {
+                result[e++] = next_edge;
+                union(subsets, x, y);
+            }
+        }
+
+        System.out.println("Edges in the MST:");
+        for (i = 0; i < e; ++i)
+            System.out.println(result[i].src + " - " + result[i].dest + " : " + result[i].weight);
+    }
+
+    public static void main(String[] args) {
+        int V = 4;
+        int E = 5;
+        day48_minimum_spanning_tree graph = new day48_minimum_spanning_tree(V, E);
+
+        graph.edges[0] = new Edge(0, 1, 10);
+        graph.edges[1] = new Edge(0, 2, 6);
+        graph.edges[2] = new Edge(0, 3, 5);
+        graph.edges[3] = new Edge(1, 3, 15);
+        graph.edges[4] = new Edge(2, 3, 4);
+
+        graph.kruskalMST();
+    }
+}
