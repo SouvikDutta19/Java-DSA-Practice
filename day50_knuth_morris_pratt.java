42111f279c4259034b1979cbac384e1bfc15fa57
Souvik_Dutta <souvikdutta491@gmail.com>
2025-08-19 08:54:23 +0530

    Added KMP string matching algorithm

---
+++ day50_knuth_morris_pratt.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class day50_knuth_morris_pratt {
+
+    void KMPSearch(String pat, String txt) {
+        int M = pat.length();
+        int N = txt.length();
+
+        int lps[] = new int[M];
+        int j = 0;
+
+        computeLPSArray(pat, M, lps);
+
+        int i = 0;
+        while (i < N) {
+            if (pat.charAt(j) == txt.charAt(i)) {
+                j++;
+                i++;
+            }
+            if (j == M) {
+                System.out.println("Found pattern at index " + (i - j));
+                j = lps[j - 1];
+            } else if (i < N && pat.charAt(j) != txt.charAt(i)) {
+                if (j != 0) j = lps[j - 1];
+                else i++;
+            }
+        }
+    }
+
+    void computeLPSArray(String pat, int M, int lps[]) {
+        int len = 0;
+        int i = 1;
+        lps[0] = 0;
+
+        while (i < M) {
+            if (pat.charAt(i) == pat.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        day50_knuth_morris_pratt kmp = new day50_knuth_morris_pratt();
+        String txt = "ABABDABACDABABCABAB";
+        String pat = "ABABCABAB";
+        kmp.KMPSearch(pat, txt);
+    }
+}
