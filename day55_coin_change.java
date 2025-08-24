4f482ebfb6a850119733d4a37c753831d4ddb59d
Souvik_Dutta <souvikdutta491@gmail.com>
2025-08-24 21:40:51 +0530

    Implemented Coin Change problem using DP in Day55

---
+++ day55_coin_change.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class day55_coin_change {
+    public static int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+
+    public static void main(String[] args) {
+        int[] coins = {1, 2, 5};
+        int amount = 11;
+        System.out.println("Minimum coins required: " + coinChange(coins, amount));
+    }
+}
