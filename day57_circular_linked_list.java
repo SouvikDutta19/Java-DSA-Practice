947301df38dd9f66d0322de0be38b74b35bf9f54
Souvik_Dutta <souvikdutta491@gmail.com>
2025-08-26 10:22:38 +0530

    Implemented Circular Linked List with insertion and traversal

---
+++ day57_circular_linked_list.java
@@ -0,0 +1,46 @@
+public class day57_circular_linked_list {
+    static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static class CircularLinkedList {
+        Node head = null, tail = null;
+
+        void insert(int data) {
+            Node newNode = new Node(data);
+            if (head == null) {
+                head = newNode;
+                tail = newNode;
+                newNode.next = head;
+            } else {
+                tail.next = newNode;
+                tail = newNode;
+                tail.next = head;
+            }
+        }
+
+        void display() {
+            if (head == null) return;
+            Node temp = head;
+            do {
+                System.out.print(temp.data + " ");
+                temp = temp.next;
+            } while (temp != head);
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        CircularLinkedList cll = new CircularLinkedList();
+        cll.insert(1);
+        cll.insert(2);
+        cll.insert(3);
+        cll.insert(4);
+        cll.display();
+    }
+}
