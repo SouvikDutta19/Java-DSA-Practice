0183eed2754127b8577fd2849e3f3db8a0152b57
Souvik_Dutta <souvikdutta491@gmail.com>
2025-08-30 09:30:29 +0530

    Added Kosaraju's algorithm to find strongly connected components in a graph

---
+++ day61_kosaraju.java
@@ -0,0 +1,83 @@
+import java.util.*;
+
+public class day61_kosaraju {
+    private int V;
+    private LinkedList<Integer>[] adj;
+
+    day61_kosaraju(int v) {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; ++i) {
+            adj[i] = new LinkedList<>();
+        }
+    }
+
+    void addEdge(int v, int w) {
+        adj[v].add(w);
+    }
+
+    void DFSUtil(int v, boolean visited[]) {
+        visited[v] = true;
+        System.out.print(v + " ");
+        for (int n : adj[v]) {
+            if (!visited[n]) {
+                DFSUtil(n, visited);
+            }
+        }
+    }
+
+    day61_kosaraju getTranspose() {
+        day61_kosaraju g = new day61_kosaraju(V);
+        for (int v = 0; v < V; v++) {
+            for (int n : adj[v]) {
+                g.adj[n].add(v);
+            }
+        }
+        return g;
+    }
+
+    void fillOrder(int v, boolean visited[], Stack<Integer> stack) {
+        visited[v] = true;
+        for (int n : adj[v]) {
+            if (!visited[n]) {
+                fillOrder(n, visited, stack);
+            }
+        }
+        stack.push(v);
+    }
+
+    void printSCCs() {
+        Stack<Integer> stack = new Stack<>();
+        boolean visited[] = new boolean[V];
+
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                fillOrder(i, visited, stack);
+            }
+        }
+
+        day61_kosaraju gr = getTranspose();
+
+        Arrays.fill(visited, false);
+
+        while (!stack.isEmpty()) {
+            int v = stack.pop();
+            if (!visited[v]) {
+                gr.DFSUtil(v, visited);
+                System.out.println();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        day61_kosaraju g = new day61_kosaraju(5);
+        g.addEdge(1, 0);
+        g.addEdge(0, 2);
+        g.addEdge(2, 1);
+        g.addEdge(0, 3);
+        g.addEdge(3, 4);
+
+        System.out.println("Strongly Connected Components:");
+        g.printSCCs();
+    }
+}
