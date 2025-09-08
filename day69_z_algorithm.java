0bee145bd4fa7b262de6f482649d67a199639ef3
Souvik_Dutta <souvikdutta491@gmail.com>
2025-09-08 08:15:07 +0530

    day69: Z-algorithm for pattern matching in linear time

---
+++ day69_z_algorithm.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class day69_z_algorithm {
+    static int[] Z(String s) {
+        int n = s.length(), l = 0, r = 0;
+        int[] z = new int[n];
+        for (int i = 1; i < n; i++) {
+            if (i <= r) z[i] = Math.min(r - i + 1, z[i - l]);
+            while (i + z[i] < n && s.charAt(z[i]) == s.charAt(i + z[i])) z[i]++;
+            if (i + z[i] - 1 > r) { l = i; r = i + z[i] - 1; }
+        }
+        return z;
+    }
+
+    public static void main(String[] args) {
+        String s = "aabxaabxcaabxaabxay";
+        System.out.println(Arrays.toString(Z(s)));
+    }
+}
