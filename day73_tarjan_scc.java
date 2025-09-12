d301518cccfea1e5ad282c7aed325aa971c9eda5
Souvik_Dutta <souvikdutta491@gmail.com>
2025-09-12 19:15:52 +0530

    day73: Tarjan’s Algorithm for Strongly Connected Components

---
+++ day73_tarjan_scc.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class day73_tarjan_scc {
+    static int time=0;
+    static void dfs(int u, List<List<Integer>> adj, int[] disc, int[] low, boolean[] stackMember, Stack<Integer> st){
+        disc[u]=low[u]=++time;
+        st.push(u); stackMember[u]=true;
+
+        for(int v: adj.get(u)){
+            if(disc[v]==-1){
+                dfs(v,adj,disc,low,stackMember,st);
+                low[u]=Math.min(low[u],low[v]);
+            } else if(stackMember[v]){
+                low[u]=Math.min(low[u],disc[v]);
+            }
+        }
+
+        if(low[u]==disc[u]){
+            while(true){
+                int w=st.pop();
+                stackMember[w]=false;
+                System.out.print(w+" ");
+                if(w==u) break;
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args){
+        int n=5;
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
+        adj.get(1).add(0); adj.get(0).add(2); adj.get(2).add(1);
+        adj.get(0).add(3); adj.get(3).add(4);
+
+        int[] disc=new int[n], low=new int[n];
+        Arrays.fill(disc,-1);
+        Arrays.fill(low,-1);
+        boolean[] stackMember=new boolean[n];
+        Stack<Integer> st=new Stack<>();
+
+        for(int i=0;i<n;i++) if(disc[i]==-1) dfs(i,adj,disc,low,stackMember,st);
+    }
+}
