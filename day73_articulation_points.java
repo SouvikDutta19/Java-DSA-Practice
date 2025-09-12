e8b1d3ef87d2b6bb813c80bc0ccefda1bab0b63c
Souvik_Dutta <souvikdutta491@gmail.com>
2025-09-12 19:16:28 +0530

    day73: Find Articulation Points in Graph using DFS

---
+++ day73_articulation_points.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class day73_articulation_points {
+    static int time=0;
+    static void dfs(int u,int parent,List<List<Integer>> adj,int[] disc,int[] low,boolean[] ap){
+        disc[u]=low[u]=++time;
+        int children=0;
+        for(int v: adj.get(u)){
+            if(disc[v]==-1){
+                children++;
+                dfs(v,u,adj,disc,low,ap);
+                low[u]=Math.min(low[u],low[v]);
+                if(parent!=-1 && low[v]>=disc[u]) ap[u]=true;
+            } else if(v!=parent){
+                low[u]=Math.min(low[u],disc[v]);
+            }
+        }
+        if(parent==-1 && children>1) ap[u]=true;
+    }
+
+    public static void main(String[] args){
+        int n=5;
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
+        adj.get(0).addAll(Arrays.asList(1,2));
+        adj.get(1).addAll(Arrays.asList(0,2));
+        adj.get(2).addAll(Arrays.asList(0,1,3,4));
+        adj.get(3).addAll(Arrays.asList(2,4));
+        adj.get(4).addAll(Arrays.asList(2,3));
+
+        int[] disc=new int[n],low=new int[n];
+        Arrays.fill(disc,-1); Arrays.fill(low,-1);
+        boolean[] ap=new boolean[n];
+        for(int i=0;i<n;i++) if(disc[i]==-1) dfs(i,-1,adj,disc,low,ap);
+
+        System.out.println("Articulation Points:");
+        for(int i=0;i<n;i++) if(ap[i]) System.out.println(i);
+    }
+}
