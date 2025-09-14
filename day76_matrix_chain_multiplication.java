6146d70e005827387f9cfcb333c85c0de9de650f
Souvik_Dutta <souvikdutta491@gmail.com>
2025-09-14 21:20:26 +0530

    day76: Matrix Chain Multiplication (DP)

---
+++ day76_matrix_chain_multiplication.java
@@ -0,0 +1,20 @@
+public class day76_matrix_chain_multiplication {
+    static int matrixChainOrder(int[] p,int n){
+        int[][] dp=new int[n][n];
+        for(int len=2;len<n;len++){
+            for(int i=1;i<n-len+1;i++){
+                int j=i+len-1;
+                dp[i][j]=Integer.MAX_VALUE;
+                for(int k=i;k<j;k++){
+                    int cost=dp[i][k]+dp[k+1][j]+p[i-1]*p[k]*p[j];
+                    dp[i][j]=Math.min(dp[i][j],cost);
+                }
+            }
+        }
+        return dp[1][n-1];
+    }
+    public static void main(String[] args){
+        int[] arr={1,2,3,4,3};
+        System.out.println("Min multiplications: "+matrixChainOrder(arr,arr.length));
+    }
+}
