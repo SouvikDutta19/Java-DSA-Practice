dd083e7945ff4b524fe680f614815ed5e51d92bd
Souvik_Dutta <souvikdutta491@gmail.com>
2025-09-16 20:29:58 +0530

    day80: Maximal Square in binary matrix using DP

---
+++ day80_maximal_square.java
@@ -0,0 +1,20 @@
+public class day80_maximal_square {
+    public static int maximalSquare(char[][] matrix) {
+        int m=matrix.length, n=matrix[0].length, max=0;
+        int[][] dp=new int[m+1][n+1];
+        for(int i=1;i<=m;i++) {
+            for(int j=1;j<=n;j++) {
+                if(matrix[i-1][j-1]=='1') {
+                    dp[i][j]=Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]))+1;
+                    max=Math.max(max,dp[i][j]);
+                }
+            }
+        }
+        return max*max;
+    }
+
+    public static void main(String[] args) {
+        char[][] mat={{'1','0','1','0','0'},{'1','0','1','1','1'},{'1','1','1','1','1'},{'1','0','0','1','0'}};
+        System.out.println("Maximal Square Area: " + maximalSquare(mat));
+    }
+}
