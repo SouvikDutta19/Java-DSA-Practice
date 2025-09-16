11a894053a33ab7707c503138a22281c81023d9d
Souvik_Dutta <souvikdutta491@gmail.com>
2025-09-16 20:31:59 +0530

    day80: Coin Change minimum coins using DP

---
+++ day80_coin_change.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class day80_coin_change {
+    public static int coinChange(int[] coins,int amount) {
+        int[] dp=new int[amount+1];
+        Arrays.fill(dp,amount+1);
+        dp[0]=0;
+        for(int coin:coins) {
+            for(int i=coin;i<=amount;i++) {
+                dp[i]=Math.min(dp[i],dp[i-coin]+1);
+            }
+        }
+        return dp[amount]>amount?-1:dp[amount];
+    }
+
+    public static void main(String[] args) {
+        int[] coins={1,2,5};
+        System.out.println("Min Coins: " + coinChange(coins,11));
+    }
+}
