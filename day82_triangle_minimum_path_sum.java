38488a17cced6e977143a4e3b93d738d91b0554e
Souvik_Dutta <souvikdutta491@gmail.com>
2025-09-18 18:51:42 +0530

    day82: Triangle Minimum Path Sum using DP

---
+++ day82_triangle_minimum_path_sum.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class day82_triangle_minimum_path_sum {
+    public static int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[] dp = new int[n + 1];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j < triangle.get(i).size(); j++) {
+                dp[j] = Math.min(dp[j], dp[j + 1]) + triangle.get(i).get(j);
+            }
+        }
+        return dp[0];
+    }
+
+    public static void main(String[] args) {
+        List<List<Integer>> triangle = new ArrayList<>();
+        triangle.add(Arrays.asList(2));
+        triangle.add(Arrays.asList(3,4));
+        triangle.add(Arrays.asList(6,5,7));
+        triangle.add(Arrays.asList(4,1,8,3));
+        System.out.println("Triangle Min Path Sum: " + minimumTotal(triangle));
+    }
+}
