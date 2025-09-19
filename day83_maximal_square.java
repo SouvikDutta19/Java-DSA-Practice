21ad5345c55adeb79f0efc5277d8afca2a236ad3
Souvik_Dutta <souvikdutta491@gmail.com>
2025-09-19 09:26:13 +0530

    Implemented DP solution for Maximal Square problem

---
+++ day83_maximal_square.java
@@ -0,0 +1,28 @@
+public class day83_maximal_square {
+    public static int maximalSquare(char[][] matrix) {
+        if (matrix.length == 0) return 0;
+        int rows = matrix.length, cols = matrix[0].length;
+        int[][] dp = new int[rows + 1][cols + 1];
+        int max = 0;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+        return max * max;
+    }
+
+    public static void main(String[] args) {
+        char[][] matrix = {
+            {'1','0','1','0','0'},
+            {'1','0','1','1','1'},
+            {'1','1','1','1','1'},
+            {'1','0','0','1','0'}
+        };
+        System.out.println("Maximal Square Area: " + maximalSquare(matrix));
+    }
+}
