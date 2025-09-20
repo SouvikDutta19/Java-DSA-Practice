05875b5a9d7008d5373b5901bf74fa2c536d4cec
Souvik_Dutta <souvikdutta491@gmail.com>
2025-09-20 12:07:57 +0530

    Added Longest Palindromic Subsequence using DP

---
+++ day85_longest_palindromic_subsequence.java
@@ -0,0 +1,19 @@
+public class day85_longest_palindromic_subsequence {
+    public static int longestPalindromeSubseq(String s) {
+        int n = s.length();
+        int[][] dp = new int[n][n];
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < n; j++) {
+                if (s.charAt(i) == s.charAt(j)) dp[i][j] = 2 + dp[i + 1][j - 1];
+                else dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[0][n - 1];
+    }
+
+    public static void main(String[] args) {
+        String s = "bbbab";
+        System.out.println("Longest Palindromic Subsequence: " + longestPalindromeSubseq(s));
+    }
+}
