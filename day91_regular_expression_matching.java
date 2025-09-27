1d5b1bc3c8414267699dc2ec7afa56e6c20f1146
Souvik_Dutta <souvikdutta491@gmail.com>
2025-09-27 22:12:09 +0530

    Implemented Regular Expression Matching using DP

---
+++ day91_regular_expression_matching.java
@@ -0,0 +1,27 @@
+public class day91_regular_expression_matching {
+    public static boolean isMatch(String s, String p) {
+        int m = s.length(), n = p.length();
+        boolean[][] dp = new boolean[m + 1][n + 1];
+        dp[0][0] = true;
+
+        for (int j = 1; j <= n; j++) {
+            if (p.charAt(j - 1) == '*') dp[0][j] = dp[0][j - 2];
+        }
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (p.charAt(j - 1) == '.' || p.charAt(j - 1) == s.charAt(i - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i][j - 2] || 
+                               (dp[i - 1][j] && (s.charAt(i - 1) == p.charAt(j - 2) || p.charAt(j - 2) == '.'));
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Regex Match: " + isMatch("aab", "c*a*b"));
+    }
+}
