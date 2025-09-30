3057f8a46fde307e835b468cc6b866d92ad9f2aa
Souvik_Dutta <souvikdutta491@gmail.com>
2025-09-30 22:53:08 +0530

    day94: Longest Repeating Subsequence using DP

---
+++ day94_longest_repeating_subsequence.java
@@ -0,0 +1,19 @@
+public class day94_longest_repeating_subsequence {
+    public static int lrs(String s) {
+        int n = s.length();
+        int[][] dp = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s.charAt(i - 1) == s.charAt(j - 1) && i != j)
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[n][n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Longest Repeating Subsequence: " + lrs("aabb"));
+    }
+}
