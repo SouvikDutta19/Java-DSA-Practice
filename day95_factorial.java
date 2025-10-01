072bd13e3cee1f40063a0efa41c0ac01d64bb341
Souvik Dutta <116168757+SouvikDutta19@users.noreply.github.com>
2025-10-01 19:05:14 +0530

    Create day95_factorial.java

---
+++ day95_factorial.java
@@ -0,0 +1,11 @@
+public class day95_factorial {
+    public static long factorial(int n) {
+        if (n == 0 || n == 1) return 1;
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+        int num = 6;
+        System.out.println("Factorial of " + num + " = " + factorial(num));
+    }
+}
