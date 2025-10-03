96a21c142b13432bcd7da92bae7fad5640a5f213
Souvik_Dutta <souvikdutta491@gmail.com>
2025-10-03 21:42:51 +0530

    Implemented Bellman-Ford algorithm for shortest path

---
+++ day97_bellmanford.java
@@ -0,0 +1,39 @@
+public class day97_bellmanford {
+    static class Edge {
+        int src, dest, weight;
+        Edge(int s, int d, int w) { src = s; dest = d; weight = w; }
+    }
+
+    static void bellmanFord(int V, int E, Edge edges[], int src) {
+        int dist[] = new int[V];
+        for (int i = 0; i < V; ++i) dist[i] = Integer.MAX_VALUE;
+        dist[src] = 0;
+
+        for (int i = 1; i < V; ++i) {
+            for (int j = 0; j < E; ++j) {
+                int u = edges[j].src, v = edges[j].dest, w = edges[j].weight;
+                if (dist[u] != Integer.MAX_VALUE && dist[u] + w < dist[v]) dist[v] = dist[u] + w;
+            }
+        }
+
+        for (int j = 0; j < E; ++j) {
+            int u = edges[j].src, v = edges[j].dest, w = edges[j].weight;
+            if (dist[u] != Integer.MAX_VALUE && dist[u] + w < dist[v]) {
+                System.out.println("Graph contains negative weight cycle");
+                return;
+            }
+        }
+
+        for (int i = 0; i < V; ++i) System.out.println("Distance from " + src + " to " + i + " = " + dist[i]);
+    }
+
+    public static void main(String[] args) {
+        int V = 5, E = 8;
+        Edge edges[] = {
+            new Edge(0,1,-1), new Edge(0,2,4), new Edge(1,2,3),
+            new Edge(1,3,2), new Edge(1,4,2), new Edge(3,2,5),
+            new Edge(3,1,1), new Edge(4,3,-3)
+        };
+        bellmanFord(V, E, edges, 0);
+    }
+}
