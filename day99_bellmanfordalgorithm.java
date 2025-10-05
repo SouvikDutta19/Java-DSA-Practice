006302a87fa1d11453374656223bc65af588cb3a
Souvik_Dutta <souvikdutta491@gmail.com>
2025-10-05 18:40:02 +0530

    Added Bellman-Ford shortest path algorithm for Day99

---
+++ day99_bellmanfordalgorithm.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Edge {
+    int src, dest, weight;
+    Edge(int s, int d, int w) { src = s; dest = d; weight = w; }
+}
+
+public class day99_bellmanfordalgorithm {
+    static void bellmanFord(int V, List<Edge> edges, int src) {
+        int[] dist = new int[V];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[src] = 0;
+
+        for (int i = 1; i < V; i++) {
+            for (Edge e : edges) {
+                if (dist[e.src] != Integer.MAX_VALUE && dist[e.src] + e.weight < dist[e.dest]) {
+                    dist[e.dest] = dist[e.src] + e.weight;
+                }
+            }
+        }
+
+        for (Edge e : edges) {
+            if (dist[e.src] != Integer.MAX_VALUE && dist[e.src] + e.weight < dist[e.dest]) {
+                System.out.println("Negative weight cycle detected!");
+                return;
+            }
+        }
+
+        for (int i = 0; i < V; i++)
+            System.out.println("Distance from " + src + " to " + i + " = " + dist[i]);
+    }
+
+    public static void main(String[] args) {
+        int V = 5;
+        List<Edge> edges = new ArrayList<>();
+        edges.add(new Edge(0, 1, -1));
+        edges.add(new Edge(0, 2, 4));
+        edges.add(new Edge(1, 2, 3));
+        edges.add(new Edge(1, 3, 2));
+        edges.add(new Edge(1, 4, 2));
+        edges.add(new Edge(3, 2, 5));
+        edges.add(new Edge(3, 1, 1));
+        edges.add(new Edge(4, 3, -3));
+
+        bellmanFord(V, edges, 0);
+    }
+}
