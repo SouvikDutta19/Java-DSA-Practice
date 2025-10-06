900c4191dcd9cdaa911df2bcc945af13054b2b1c
Souvik_Dutta <souvikdutta491@gmail.com>
2025-10-06 19:52:54 +0530

    Added Longest Increasing Subsequence (DP O(n²)) implementation for Day100

---
+++ day100_longestincreasingsubsequence.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class day100_longestincreasingsubsequence {
+    static int lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j])
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+            }
+        }
+        int max = 0;
+        for (int i : dp) max = Math.max(max, i);
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {10, 9, 2, 5, 3, 7, 101, 18};
+        System.out.println("Length of LIS: " + lengthOfLIS(nums));
+    }
+}
