eb83e5f13f46cc010b0fd0942c48d261ea9fb4f5
Souvik_Dutta <souvikdutta491@gmail.com>
2025-10-07 19:48:42 +0530

    Added dynamic programming solution for Longest Common Subsequence

---
+++ day101_lcs.java
@@ -0,0 +1,21 @@
+public class day101_lcs {
+    static int lcs(String X, String Y, int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0)
+                    dp[i][j] = 0;
+                else if (X.charAt(i - 1) == Y.charAt(j - 1))
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        String s1 = "AGGTAB", s2 = "GXTXAYB";
+        System.out.println("Length of LCS is: " + lcs(s1, s2, s1.length(), s2.length()));
+    }
+}
