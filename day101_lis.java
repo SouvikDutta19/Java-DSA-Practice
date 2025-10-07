7df1a3bdeaaefebf560b6078ad100d45c74e6eef
Souvik_Dutta <souvikdutta491@gmail.com>
2025-10-07 19:56:07 +0530

    Added dynamic programming solution for Longest Increasing Subsequence

---
+++ day101_lis.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class day101_lis {
+    static int lis(int arr[], int n) {
+        int lis[] = new int[n];
+        Arrays.fill(lis, 1);
+        for (int i = 1; i < n; i++)
+            for (int j = 0; j < i; j++)
+                if (arr[i] > arr[j] && lis[i] < lis[j] + 1)
+                    lis[i] = lis[j] + 1;
+        return Arrays.stream(lis).max().getAsInt();
+    }
+
+    public static void main(String args[]) {
+        int arr[] = {10, 22, 9, 33, 21, 50, 41, 60};
+        System.out.println("Length of LIS = " + lis(arr, arr.length));
+    }
+}
