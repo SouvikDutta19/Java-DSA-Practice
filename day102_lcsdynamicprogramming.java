1303cf711867d15f940129c24710c8292120e8e2
Souvik_Dutta <souvikdutta491@gmail.com>
2025-10-08 17:53:35 +0530

    Implemented Longest Common Subsequence using Dynamic Programming

---
+++ day102_lcsdynamicprogramming.java
@@ -0,0 +1,20 @@
+public class day102_lcsdynamicprogramming {
+    public static int lcs(String X, String Y, int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (X.charAt(i - 1) == Y.charAt(j - 1))
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        String X = "AGGTAB";
+        String Y = "GXTXAYB";
+        System.out.println("Length of LCS: " + lcs(X, Y, X.length(), Y.length()));
+    }
+}
