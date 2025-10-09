421a60b568493dddcd29e255c03afd92b767138a
Souvik_Dutta <souvikdutta491@gmail.com>
2025-10-09 20:57:18 +0530

    Implemented Topological Sort using DFS

---
+++ day103_topologicalsort.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+public class day103_topologicalsort {
+    private int V;
+    private LinkedList<Integer>[] adj;
+
+    public day103_topologicalsort(int v) {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; ++i)
+            adj[i] = new LinkedList<>();
+    }
+
+    void addEdge(int v, int w) {
+        adj[v].add(w);
+    }
+
+    void topologicalSortUtil(int v, boolean[] visited, Stack<Integer> stack) {
+        visited[v] = true;
+        for (int i : adj[v])
+            if (!visited[i])
+                topologicalSortUtil(i, visited, stack);
+        stack.push(v);
+    }
+
+    void topologicalSort() {
+        Stack<Integer> stack = new Stack<>();
+        boolean[] visited = new boolean[V];
+        for (int i = 0; i < V; i++)
+            if (!visited[i])
+                topologicalSortUtil(i, visited, stack);
+        System.out.println("Topological Sort Order:");
+        while (!stack.isEmpty())
+            System.out.print(stack.pop() + " ");
+    }
+
+    public static void main(String[] args) {
+        day103_topologicalsort g = new day103_topologicalsort(6);
+        g.addEdge(5, 2);
+        g.addEdge(5, 0);
+        g.addEdge(4, 0);
+        g.addEdge(4, 1);
+        g.addEdge(2, 3);
+        g.addEdge(3, 1);
+        g.topologicalSort();
+    }
+}
