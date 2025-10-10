a4ad2e5f80b1d3cd74a0689297378c975c67e32c
Souvik_Dutta <souvikdutta491@gmail.com>
2025-10-10 17:59:42 +0530

    Solved 0/1 Knapsack problem using bottom-up dynamic programming approach (day104_knapsack_problem.java)

---
+++ day104_knapsack_problem.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+// 0/1 Knapsack problem using Dynamic Programming
+public class day104_knapsack_problem {
+    public static int knapSack(int W, int wt[], int val[], int n) {
+        int[][] dp = new int[n + 1][W + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int w = 1; w <= W; w++) {
+                if (wt[i - 1] <= w)
+                    dp[i][w] = Math.max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w]);
+                else
+                    dp[i][w] = dp[i - 1][w];
+            }
+        }
+        return dp[n][W];
+    }
+
+    public static void main(String[] args) {
+        int val[] = {60, 100, 120};
+        int wt[] = {10, 20, 30};
+        int W = 50;
+        System.out.println("Maximum value in Knapsack = " + knapSack(W, wt, val, val.length));
+    }
+}
