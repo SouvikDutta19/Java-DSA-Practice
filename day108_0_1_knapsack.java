41e137e5f863d001215d025cd24df9c691a2900d
Souvik_Dutta <souvikdutta491@gmail.com>
2025-10-14 09:37:27 +0530

    Implemented 0/1 Knapsack problem using Dynamic Programming (day108_0_1_knapsack.java)

---
+++ day108_0_1_knapsack.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+// 0/1 Knapsack Problem using Dynamic Programming
+public class day108_0_1_knapsack {
+    static int knapsack(int W, int wt[], int val[], int n) {
+        int[][] dp = new int[n + 1][W + 1];
+
+        for (int i = 1; i <= n; i++) {
+            for (int w = 1; w <= W; w++) {
+                if (wt[i - 1] <= w)
+                    dp[i][w] = Math.max(val[i - 1] + dp[i - 1][w - wt[i - 1]], dp[i - 1][w]);
+                else
+                    dp[i][w] = dp[i - 1][w];
+            }
+        }
+        return dp[n][W];
+    }
+
+    public static void main(String[] args) {
+        int[] val = {60, 100, 120};
+        int[] wt = {10, 20, 30};
+        int W = 50;
+        System.out.println("Maximum profit = " + knapsack(W, wt, val, val.length));
+    }
+}
