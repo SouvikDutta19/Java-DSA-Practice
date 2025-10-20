3e7e1f42a90fb430034ff3a9307ae10d158ee108
Souvik_Dutta <souvikdutta491@gmail.com>
2025-10-20 19:51:48 +0530

    Added Edit Distance Dynamic Programming solution (Day113)

---
+++ day113_edit_distance_dp.java
@@ -0,0 +1,30 @@
+// Program to find Edit Distance (Levenshtein Distance) using Dynamic Programming
+
+public class day113_edit_distance_dp {
+    public static int minDistance(String word1, String word2) {
+        int m = word1.length(), n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++)
+            dp[i][0] = i;
+        for (int j = 0; j <= n; j++)
+            dp[0][j] = j;
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1))
+                    dp[i][j] = dp[i - 1][j - 1];
+                else
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j - 1],
+                                   Math.min(dp[i - 1][j], dp[i][j - 1]));
+            }
+        }
+
+        return dp[m][n];
+    }
+
+    public static void main(String[] args) {
+        String w1 = "horse", w2 = "ros";
+        System.out.println("Edit distance between " + w1 + " and " + w2 + ": " + minDistance(w1, w2));
+    }
+}
