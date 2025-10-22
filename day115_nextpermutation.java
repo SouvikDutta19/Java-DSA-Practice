faff70768aec530bd3b4f1d62d9639951ed6d642
Souvik Dutta <116168757+SouvikDutta19@users.noreply.github.com>
2025-10-22 19:06:09 +0530

    Create day115_nextpermutation.java

---
+++ day115_nextpermutation.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class day115_nextpermutation {
+    public static void nextPermutation(int[] nums) {
+        int i = nums.length - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) i--;
+        if (i >= 0) {
+            int j = nums.length - 1;
+            while (nums[j] <= nums[i]) j--;
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1, nums.length - 1);
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i]; nums[i] = nums[j]; nums[j] = temp;
+    }
+
+    private static void reverse(int[] nums, int start, int end) {
+        while (start < end) swap(nums, start++, end--);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        nextPermutation(nums);
+        System.out.println("Next permutation: " + Arrays.toString(nums));
+    }
+}
