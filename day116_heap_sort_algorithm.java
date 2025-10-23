30942a9f2750e8ea4a5b5695ddfbb2b2e6f2234f
Souvik_Dutta <souvikdutta491@gmail.com>
2025-10-23 18:36:40 +0530

    Implemented Heap Sort algorithm using max-heap (day116_heap_sort_algorithm.java)

---
+++ day116_heap_sort_algorithm.java
@@ -0,0 +1,40 @@
+public class day116_heap_sort_algorithm {
+
+    static void heapify(int[] arr, int n, int i) {
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+
+        if (largest != i) {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+            heapify(arr, n, largest);
+        }
+    }
+
+    static void heapSort(int[] arr) {
+        int n = arr.length;
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+        for (int i = n - 1; i > 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            heapify(arr, i, 0);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 6, 7};
+        heapSort(arr);
+        System.out.println("Sorted array: ");
+        for (int val : arr)
+            System.out.print(val + " ");
+    }
+}
