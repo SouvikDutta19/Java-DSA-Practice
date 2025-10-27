fc0ac292280b7fbe98b1c774a8897d55edbd7943
Souvik_Dutta <souvikdutta491@gmail.com>
2025-10-27 19:32:23 +0530

    Added DP-based Wildcard Matching algorithm - day120_wildcardmatching.java

---
+++ day120_wildcardmatching.java
@@ -0,0 +1,28 @@
+// day120_wildcardmatching.java
+public class day120_wildcardmatching {
+    public boolean isMatch(String s, String p) {
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        dp[0][0] = true;
+
+        for (int j = 1; j <= p.length(); j++) {
+            if (p.charAt(j - 1) == '*') dp[0][j] = dp[0][j - 1];
+        }
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 1; j <= p.length(); j++) {
+                if (p.charAt(j - 1) == '?' || p.charAt(j - 1) == s.charAt(i - 1))
+                    dp[i][j] = dp[i - 1][j - 1];
+                else if (p.charAt(j - 1) == '*')
+                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+            }
+        }
+
+        return dp[s.length()][p.length()];
+    }
+
+    public static void main(String[] args) {
+        day120_wildcardmatching obj = new day120_wildcardmatching();
+        System.out.println(obj.isMatch("adceb", "*a*b"));
+        System.out.println(obj.isMatch("cb", "?a"));
+    }
+}
