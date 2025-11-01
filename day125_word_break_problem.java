bf162ad84bd84440b0c8d115b62f5ddd9f1aa83e
Souvik_Dutta <souvikdutta491@gmail.com>
2025-11-01 10:19:10 +0530

    Implemented Word Break problem using Dynamic Programming (day125_word_break_problem.java)

---
+++ day125_word_break_problem.java
@@ -0,0 +1,25 @@
+// day125_word_break_problem.java
+import java.util.*;
+
+public class day125_word_break_problem {
+    static boolean wordBreak(String s, List<String> dict) {
+        Set<String> set = new HashSet<>(dict);
+        boolean[] dp = new boolean[s.length() + 1];
+        dp[0] = true;
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && set.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+
+    public static void main(String[] args) {
+        List<String> dict = Arrays.asList("leet", "code", "is", "fun");
+        System.out.println("Can be segmented: " + wordBreak("leetcode", dict));
+    }
+}
