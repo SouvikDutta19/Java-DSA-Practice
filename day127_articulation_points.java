004ecb4c20c78c01d5d0d3f5daf041da13ea3750
Souvik_Dutta <souvikdutta491@gmail.com>
2025-11-03 20:42:31 +0530

    Implemented Tarjan’s algorithm to find Articulation Points (day127_articulation_points.java)

---
+++ day127_articulation_points.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+public class day127_articulation_points {
+    static int time = 0;
+
+    static void dfsAP(int u, boolean[] visited, int[] disc, int[] low, int[] parent, boolean[] ap, List<List<Integer>> adj) {
+        int children = 0;
+        visited[u] = true;
+        disc[u] = low[u] = ++time;
+
+        for (int v : adj.get(u)) {
+            if (!visited[v]) {
+                children++;
+                parent[v] = u;
+                dfsAP(v, visited, disc, low, parent, ap, adj);
+
+                low[u] = Math.min(low[u], low[v]);
+
+                if (parent[u] == -1 && children > 1)
+                    ap[u] = true;
+
+                if (parent[u] != -1 && low[v] >= disc[u])
+                    ap[u] = true;
+            } else if (v != parent[u])
+                low[u] = Math.min(low[u], disc[v]);
+        }
+    }
+
+    static void findArticulationPoints(List<List<Integer>> adj, int V) {
+        boolean[] visited = new boolean[V];
+        int[] disc = new int[V];
+        int[] low = new int[V];
+        int[] parent = new int[V];
+        boolean[] ap = new boolean[V];
+        Arrays.fill(parent, -1);
+
+        for (int i = 0; i < V; i++)
+            if (!visited[i])
+                dfsAP(i, visited, disc, low, parent, ap, adj);
+
+        System.out.println("Articulation Points:");
+        for (int i = 0; i < V; i++)
+            if (ap[i])
+                System.out.print(i + " ");
+    }
+
+    public static void main(String[] args) {
+        int V = 5;
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++)
+            adj.add(new ArrayList<>());
+
+        adj.get(1).add(0);
+        adj.get(0).add(1);
+        adj.get(0).add(2);
+        adj.get(2).add(0);
+        adj.get(0).add(3);
+        adj.get(3).add(0);
+        adj.get(3).add(4);
+        adj.get(4).add(3);
+
+        findArticulationPoints(adj, V);
+    }
+}
