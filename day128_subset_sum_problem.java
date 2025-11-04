6af49374c49352ee6ca4eef3e115e7e80747063e
Souvik_Dutta <souvikdutta491@gmail.com>
2025-11-04 19:27:08 +0530

    Implemented Subset Sum Problem using DP table (day128_subset_sum_problem.java)

---
+++ day128_subset_sum_problem.java
@@ -0,0 +1,26 @@
+public class day128_subset_sum_problem {
+
+    static boolean subsetSum(int[] arr, int n, int sum) {
+        boolean[][] dp = new boolean[n + 1][sum + 1];
+
+        for (int i = 0; i <= n; i++)
+            dp[i][0] = true;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= sum; j++) {
+                if (arr[i - 1] <= j)
+                    dp[i][j] = dp[i - 1][j - arr[i - 1]] || dp[i - 1][j];
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+
+        return dp[n][sum];
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 34, 4, 12, 5, 2};
+        int sum = 9;
+        System.out.println("Subset with given sum exists: " + subsetSum(arr, arr.length, sum));
+    }
+}
