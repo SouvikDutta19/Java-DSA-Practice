f4d25c5c7997c16061414e824b9f25cd2c047f76
Souvik_Dutta <souvikdutta491@gmail.com>
2025-11-04 19:26:52 +0530

    Solved Coin Change problem using 1D DP (day128_coin_change_problem.java)

---
+++ day128_coin_change_problem.java
@@ -0,0 +1,21 @@
+public class day128_coin_change_problem {
+
+    static int countWays(int[] coins, int n, int sum) {
+        int[] dp = new int[sum + 1];
+        dp[0] = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = coins[i]; j <= sum; j++) {
+                dp[j] += dp[j - coins[i]];
+            }
+        }
+
+        return dp[sum];
+    }
+
+    public static void main(String[] args) {
+        int[] coins = {1, 2, 5};
+        int sum = 11;
+        System.out.println("Number of ways to make change: " + countWays(coins, coins.length, sum));
+    }
+}
