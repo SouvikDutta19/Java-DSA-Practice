d9caf3ae74635bd291cc73bb3f993985e26f9ab9
Souvik_Dutta <souvikdutta491@gmail.com>
2025-11-05 18:02:37 +0530

    Added KMP String Pattern Matching Algorithm - Day129

---
+++ day129_kmp_pattern_matching.java
@@ -0,0 +1,46 @@
+public class day129_kmp_pattern_matching {
+
+    void KMPSearch(String pat, String txt) {
+        int M = pat.length();
+        int N = txt.length();
+        int[] lps = new int[M];
+        computeLPSArray(pat, M, lps);
+
+        int i = 0, j = 0;
+        while (i < N) {
+            if (pat.charAt(j) == txt.charAt(i)) {
+                i++;
+                j++;
+            }
+            if (j == M) {
+                System.out.println("Found pattern at index " + (i - j));
+                j = lps[j - 1];
+            } else if (i < N && pat.charAt(j) != txt.charAt(i)) {
+                if (j != 0)
+                    j = lps[j - 1];
+                else
+                    i++;
+            }
+        }
+    }
+
+    void computeLPSArray(String pat, int M, int[] lps) {
+        int len = 0, i = 1;
+        lps[0] = 0;
+        while (i < M) {
+            if (pat.charAt(i) == pat.charAt(len))
+                lps[i++] = ++len;
+            else if (len != 0)
+                len = lps[len - 1];
+            else
+                lps[i++] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        day129_kmp_pattern_matching kmp = new day129_kmp_pattern_matching();
+        String txt = "ABABDABACDABABCABAB";
+        String pat = "ABABCABAB";
+        kmp.KMPSearch(pat, txt);
+    }
+}
