888592fac76d852cb62204a6bed764b9bda8d0ab
Souvik_Dutta <souvikdutta491@gmail.com>
2025-11-07 19:03:47 +0530

    Added Floyd-Warshall algorithm for all-pairs shortest paths (day131_floyd_warshall.java)

---
+++ day131_floyd_warshall.java
@@ -0,0 +1,45 @@
+// day131_floyd_warshall.java
+// Floyd-Warshall algorithm for All-Pairs Shortest Paths
+
+public class day131_floyd_warshall {
+    final static int INF = 99999;
+
+    void floydWarshall(int graph[][], int V) {
+        int dist[][] = new int[V][V];
+
+        for (int i = 0; i < V; i++)
+            for (int j = 0; j < V; j++)
+                dist[i][j] = graph[i][j];
+
+        for (int k = 0; k < V; k++)
+            for (int i = 0; i < V; i++)
+                for (int j = 0; j < V; j++)
+                    if (dist[i][k] + dist[k][j] < dist[i][j])
+                        dist[i][j] = dist[i][k] + dist[k][j];
+
+        printSolution(dist, V);
+    }
+
+    void printSolution(int dist[][], int V) {
+        System.out.println("Shortest distances between every pair of vertices:");
+        for (int i = 0; i < V; ++i) {
+            for (int j = 0; j < V; ++j) {
+                if (dist[i][j] == INF)
+                    System.out.print("INF ");
+                else
+                    System.out.print(dist[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int graph[][] = {
+                {0, 5, INF, 10},
+                {INF, 0, 3, INF},
+                {INF, INF, 0, 1},
+                {INF, INF, INF, 0}
+        };
+        new day131_floyd_warshall().floydWarshall(graph, 4);
+    }
+}
