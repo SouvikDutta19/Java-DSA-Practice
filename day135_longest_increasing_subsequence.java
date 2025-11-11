04533fc30387e05f982f253bfc6e380ad41c6b4a
Souvik_Dutta <souvikdutta491@gmail.com>
2025-11-11 20:20:23 +0530

    Added Longest Increasing Subsequence (O(n log n)) - day135_longest_increasing_subsequence.java

---
+++ day135_longest_increasing_subsequence.java
@@ -0,0 +1,22 @@
+// day135_longest_increasing_subsequence.java
+// O(n log n) patience sorting method
+
+import java.util.*;
+
+public class day135_longest_increasing_subsequence {
+    public static int lengthOfLIS(int[] nums) {
+        ArrayList<Integer> tails = new ArrayList<>();
+        for (int x : nums) {
+            int idx = Collections.binarySearch(tails, x);
+            if (idx < 0) idx = -idx - 1;
+            if (idx == tails.size()) tails.add(x);
+            else tails.set(idx, x);
+        }
+        return tails.size();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10,9,2,5,3,7,101,18};
+        System.out.println("Length of LIS: " + lengthOfLIS(arr));
+    }
+}
