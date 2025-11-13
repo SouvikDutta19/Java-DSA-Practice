05de15a0f83a3306ef89b50106ede0e3e81a9b6f
Souvik_Dutta <souvikdutta491@gmail.com>
2025-11-13 18:55:37 +0530

    Added recursive validator to check if a binary tree is a valid BST - Day137

---
+++ day137_validate_binary_search_tree.java
@@ -0,0 +1,25 @@
+// day137_validate_binary_search_tree.java
+class TreeNode {
+    int val;
+    TreeNode left, right;
+    TreeNode(int val) { this.val = val; }
+}
+
+public class day137_validate_binary_search_tree {
+    public static boolean isValidBST(TreeNode root) {
+        return validate(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private static boolean validate(TreeNode node, long min, long max) {
+        if (node == null) return true;
+        if (node.val <= min || node.val >= max) return false;
+        return validate(node.left, min, node.val) && validate(node.right, node.val, max);
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(2);
+        root.left = new TreeNode(1);
+        root.right = new TreeNode(3);
+        System.out.println("Is Valid BST: " + isValidBST(root));
+    }
+}
