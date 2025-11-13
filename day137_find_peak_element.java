f88c036aa325d920de29ce057a9c908f4a22865b
Souvik_Dutta <souvikdutta491@gmail.com>
2025-11-13 18:52:11 +0530

    Added binary search based solution to find peak element in an array - Day137

---
+++ day137_find_peak_element.java
@@ -0,0 +1,19 @@
+// day137_find_peak_element.java
+public class day137_find_peak_element {
+    public static int findPeakElement(int[] nums) {
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[mid + 1])
+                right = mid;
+            else
+                left = mid + 1;
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+        System.out.println("Peak Element Index: " + findPeakElement(nums));
+    }
+}
