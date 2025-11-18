54531c48190b0f95c5cae49604fafbab4e2b2239
Souvik_Dutta <souvikdutta491@gmail.com>
2025-11-18 20:09:52 +0530

    Added longest consecutive sequence using hashing (day140)

---
+++ day140_longest_consecutive_sequence.java
@@ -0,0 +1,33 @@
+// day140_longest_consecutive_sequence.java
+
+import java.util.*;
+
+public class day140_longest_consecutive_sequence {
+
+    public static int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int n : nums) set.add(n);
+
+        int longest = 0;
+
+        for (int n : nums) {
+            if (!set.contains(n - 1)) {  // start of sequence
+                int current = n;
+                int streak = 1;
+
+                while (set.contains(current + 1)) {
+                    current++;
+                    streak++;
+                }
+
+                longest = Math.max(longest, streak);
+            }
+        }
+        return longest;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {100, 4, 200, 1, 3, 2};
+        System.out.println(longestConsecutive(arr));
+    }
+}
