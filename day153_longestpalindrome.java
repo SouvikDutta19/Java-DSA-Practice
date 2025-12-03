b92da0917aea72119cba2197892d966ff9c03e08
Souvik Dutta <116168757+SouvikDutta19@users.noreply.github.com>
2025-12-03 22:44:53 +0530

    Create day153_longestpalindrome.java

---
+++ day153_longestpalindrome.java
@@ -0,0 +1,34 @@
+public class day153_longestpalindrome {
+
+    public static String longestPalindrome(String s) {
+        if (s.length() <= 1) return s;
+
+        int start = 0, maxLen = 1;
+
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expand(s, i, i);
+            int len2 = expand(s, i, i + 1);
+            int len = Math.max(len1, len2);
+
+            if (len > maxLen) {
+                maxLen = len;
+                start = i - (len - 1) / 2;
+            }
+        }
+
+        return s.substring(start, start + maxLen);
+    }
+
+    private static int expand(String s, int left, int right) {
+        while (left >= 0 && right < s.length() && s.charAt(left) == s.charAt(right)) {
+            left--;
+            right++;
+        }
+        return right - left - 1;
+    }
+
+    public static void main(String[] args) {
+        String s = "babad";
+        System.out.println("Longest Palindromic Substring: " + longestPalindrome(s));
+    }
+}
