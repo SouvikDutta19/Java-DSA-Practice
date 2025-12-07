6d7956c5ee7bef9ed1492a4fe90c16fc358c298b
Souvik Dutta <116168757+SouvikDutta19@users.noreply.github.com>
2025-12-07 21:34:58 +0530

    Create day157_find_duplicate_characters.java

---
+++ day157_find_duplicate_characters.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class day157_find_duplicate_characters {
+
+    public static void main(String[] args) {
+        String str = "programming";
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (char c : str.toCharArray())
+            map.put(c, map.getOrDefault(c, 0) + 1);
+
+        System.out.println("Duplicate characters:");
+        for (char c : map.keySet()) {
+            if (map.get(c) > 1)
+                System.out.println(c + " = " + map.get(c));
+        }
+    }
+}
