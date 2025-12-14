390ad182303df195939eff4d1feea623523d8763
Souvik Dutta <116168757+SouvikDutta19@users.noreply.github.com>
2025-12-14 22:59:54 +0530

    Create day164_disjoint_set_union.java

---
+++ day164_disjoint_set_union.java
@@ -0,0 +1,43 @@
+public class day164_disjoint_set_union {
+
+    static class DSU {
+        int[] parent, rank;
+
+        DSU(int n) {
+            parent = new int[n];
+            rank = new int[n];
+            for (int i = 0; i < n; i++)
+                parent[i] = i;
+        }
+
+        int find(int x) {
+            if (parent[x] != x)
+                parent[x] = find(parent[x]);
+            return parent[x];
+        }
+
+        void union(int x, int y) {
+            int px = find(x);
+            int py = find(y);
+
+            if (px == py) return;
+
+            if (rank[px] < rank[py])
+                parent[px] = py;
+            else if (rank[px] > rank[py])
+                parent[py] = px;
+            else {
+                parent[py] = px;
+                rank[px]++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        DSU dsu = new DSU(5);
+        dsu.union(0, 1);
+        dsu.union(1, 2);
+
+        System.out.println(dsu.find(0) == dsu.find(2));
+    }
+}
