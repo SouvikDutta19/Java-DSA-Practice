f360c74fafd1dc36d4ff1ddec2c4a30a202be4af
Souvik Dutta <116168757+SouvikDutta19@users.noreply.github.com>
2025-12-16 23:10:53 +0530

    Create day166_merge_intervals.java

---
+++ day166_merge_intervals.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class day166_merge_intervals {
+
+    public static int[][] merge(int[][] intervals) {
+        if (intervals.length == 0) return new int[0][0];
+
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+        List<int[]> result = new ArrayList<>();
+
+        int[] current = intervals[0];
+
+        for (int i = 1; i < intervals.length; i++) {
+            if (current[1] >= intervals[i][0]) {
+                current[1] = Math.max(current[1], intervals[i][1]);
+            } else {
+                result.add(current);
+                current = intervals[i];
+            }
+        }
+        result.add(current);
+
+        return result.toArray(new int[result.size()][]);
+    }
+
+    public static void main(String[] args) {
+        int[][] intervals = {{1,3},{2,6},{8,10},{15,18}};
+
+        int[][] res = merge(intervals);
+        for (int[] in : res)
+            System.out.println(in[0] + " " + in[1]);
+    }
+}
