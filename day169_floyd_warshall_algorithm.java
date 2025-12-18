00c0f3b622046e6901821757c2488b7c51aec4a5
Souvik Dutta <116168757+SouvikDutta19@users.noreply.github.com>
2025-12-18 22:36:43 +0530

    Create day169_floyd_warshall_algorithm.java

---
+++ day169_floyd_warshall_algorithm.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class day169_floyd_warshall_algorithm {
+
+    static final int INF = 99999;
+
+    public static void floydWarshall(int[][] graph) {
+        int V = graph.length;
+        int[][] dist = new int[V][V];
+
+        for (int i = 0; i < V; i++)
+            dist[i] = graph[i].clone();
+
+        for (int k = 0; k < V; k++) {
+            for (int i = 0; i < V; i++) {
+                for (int j = 0; j < V; j++) {
+                    if (dist[i][k] + dist[k][j] < dist[i][j])
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+
+        System.out.println("All Pairs Shortest Distances:");
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][j] == INF)
+                    System.out.print("INF ");
+                else
+                    System.out.print(dist[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] graph = {
+                {0, 5, INF, 10},
+                {INF, 0, 3, INF},
+                {INF, INF, 0, 1},
+                {INF, INF, INF, 0}
+        };
+
+        floydWarshall(graph);
+    }
+}
