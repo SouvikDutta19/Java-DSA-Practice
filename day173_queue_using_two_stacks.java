459e4451508b769eb69f55cb73a0f966f5cc6dd3
Souvik Dutta <116168757+SouvikDutta19@users.noreply.github.com>
2025-12-29 22:20:30 +0530

    Create day173_queue_using_two_stacks.java

---
+++ day173_queue_using_two_stacks.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class day173_queue_using_two_stacks {
+
+    static class MyQueue {
+        Stack<Integer> s1 = new Stack<>();
+        Stack<Integer> s2 = new Stack<>();
+
+        void push(int x) {
+            s1.push(x);
+        }
+
+        int pop() {
+            if(s2.isEmpty())
+                while(!s1.isEmpty())
+                    s2.push(s1.pop());
+            return s2.pop();
+        }
+
+        int peek() {
+            if(s2.isEmpty())
+                while(!s1.isEmpty())
+                    s2.push(s1.pop());
+            return s2.peek();
+        }
+
+        boolean empty() {
+            return s1.isEmpty() && s2.isEmpty();
+        }
+    }
+
+    public static void main(String[] args) {
+        MyQueue q = new MyQueue();
+        q.push(10);
+        q.push(20);
+        q.push(30);
+        System.out.println(q.pop());
+    }
+}
