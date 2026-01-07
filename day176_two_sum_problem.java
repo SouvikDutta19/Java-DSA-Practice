f8db3c86bf90788372f330ac2ab2b0a3d63823bd
Souvik Dutta <116168757+SouvikDutta19@users.noreply.github.com>
2026-01-07 22:42:46 +0530

    Create day176_two_sum_problem.java

---
+++ day176_two_sum_problem.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class day176_two_sum_problem {
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int rem = target - nums[i];
+            if (map.containsKey(rem))
+                return new int[]{map.get(rem), i};
+            map.put(nums[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int[] res = twoSum(nums, 9);
+        System.out.println(res[0] + " " + res[1]);
+    }
+}
