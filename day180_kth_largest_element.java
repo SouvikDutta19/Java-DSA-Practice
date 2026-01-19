0a0c04a50f776e1bd0665158564ff2cc717b3d52
Souvik Dutta <116168757+SouvikDutta19@users.noreply.github.com>
2026-01-19 23:20:34 +0530

    Create day180_kth_largest_element.java

---
+++ day180_kth_largest_element.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class day180_kth_largest_element {
+
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int n : nums) {
+            pq.add(n);
+            if (pq.size() > k) pq.poll();
+        }
+        return pq.peek();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3,2,1,5,6,4};
+        System.out.println(findKthLargest(arr, 2));
+    }
+}
