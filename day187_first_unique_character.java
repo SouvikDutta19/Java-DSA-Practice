48308d0acfc15ba806dd41ed4ea86e679b34c483
Souvik Dutta <116168757+SouvikDutta19@users.noreply.github.com>
2026-02-09 22:50:07 +0530

    Create day187_first_unique_character.java

---
+++ day187_first_unique_character.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+
+public class Day187FirstUniqueCharacter {
+
+    public static int firstUniqChar(String s) {
+        HashMap<Character, Integer> count = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (count.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String s = "leetcode";
+        System.out.println(firstUniqChar(s));
+    }
+}
