41d1634f306a9f91a25828a606fc58cd12b9d120
Souvik Dutta <116168757+SouvikDutta19@users.noreply.github.com>
2026-02-19 10:21:53 +0530

    Create day190_course_schedule.java

---
+++ day190_course_schedule.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Day190CourseSchedule {
+
+    public static boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+        int[] indegree = new int[numCourses];
+
+        for (int i = 0; i < numCourses; i++)
+            graph.add(new ArrayList<>());
+
+        for (int[] pre : prerequisites) {
+            graph.get(pre[1]).add(pre[0]);
+            indegree[pre[0]]++;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < numCourses; i++)
+            if (indegree[i] == 0)
+                queue.add(i);
+
+        int count = 0;
+
+        while (!queue.isEmpty()) {
+            int course = queue.poll();
+            count++;
+
+            for (int next : graph.get(course)) {
+                indegree[next]--;
+                if (indegree[next] == 0)
+                    queue.add(next);
+            }
+        }
+
+        return count == numCourses;
+    }
+
+    public static void main(String[] args) {
+        int[][] prerequisites = {{1,0}};
+        System.out.println(canFinish(2, prerequisites));
+    }
+}
