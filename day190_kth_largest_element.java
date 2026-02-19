76b2baa69a4d9b156a5928518b2a998ae07c12ad
Souvik Dutta <116168757+SouvikDutta19@users.noreply.github.com>
2026-02-19 10:20:17 +0530

    Create day190_kth_largest_element.java

---
+++ day190_kth_largest_element.java
@@ -0,0 +1,22 @@
+import java.util.PriorityQueue;
+
+public class Day190KthLargestElement {
+
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+
+        for (int num : nums) {
+            minHeap.add(num);
+            if (minHeap.size() > k) {
+                minHeap.poll();
+            }
+        }
+
+        return minHeap.peek();
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3,2,1,5,6,4};
+        System.out.println(findKthLargest(nums, 2));
+    }
+}
