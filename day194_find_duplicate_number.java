3faab961f229e7bd78dddf1f3bc8c946be68728c
Souvik Dutta <116168757+SouvikDutta19@users.noreply.github.com>
2026-03-03 12:24:18 +0530

    Create day194_find_duplicate_number.java

---
+++ day194_find_duplicate_number.java
@@ -0,0 +1,21 @@
+public class Day194FindDuplicateNumber {
+
+    public static int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while (slow != fast);
+
+        slow = nums[0];
+
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+}
