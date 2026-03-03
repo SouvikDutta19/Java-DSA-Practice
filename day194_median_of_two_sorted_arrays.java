4e361cf3663d8a71d1ff4d820a8d50a9af0dfcdc
Souvik Dutta <116168757+SouvikDutta19@users.noreply.github.com>
2026-03-03 12:25:16 +0530

    Create day194_median_of_two_sorted_arrays.java

---
+++ day194_median_of_two_sorted_arrays.java
@@ -0,0 +1,23 @@
+public class Day194MedianOfTwoSortedArrays {
+
+    public static double findMedian(int[] nums1, int[] nums2) {
+        int[] merged = new int[nums1.length + nums2.length];
+        int i = 0, j = 0, k = 0;
+
+        while (i < nums1.length && j < nums2.length)
+            merged[k++] = nums1[i] < nums2[j] ? nums1[i++] : nums2[j++];
+
+        while (i < nums1.length)
+            merged[k++] = nums1[i++];
+
+        while (j < nums2.length)
+            merged[k++] = nums2[j++];
+
+        int n = merged.length;
+
+        if (n % 2 == 0)
+            return (merged[n/2 - 1] + merged[n/2]) / 2.0;
+
+        return merged[n/2];
+    }
+}
