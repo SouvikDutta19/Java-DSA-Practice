cc601783965154ce4966ca87d4f05ce07582ec15
Souvik Dutta <116168757+SouvikDutta19@users.noreply.github.com>
2026-03-04 18:02:17 +0530

    Create day196_trie_implementation.java

---
+++ day196_trie_implementation.java
@@ -0,0 +1,30 @@
+class TrieNode {
+    TrieNode[] children = new TrieNode[26];
+    boolean isEndOfWord;
+}
+
+class Trie {
+    private final TrieNode root = new TrieNode();
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            if (node.children[index] == null)
+                node.children[index] = new TrieNode();
+            node = node.children[index];
+        }
+        node.isEndOfWord = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+            if (node.children[index] == null)
+                return false;
+            node = node.children[index];
+        }
+        return node.isEndOfWord;
+    }
+}
