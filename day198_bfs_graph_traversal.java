e51dad19bfa4b44341899143c7e32e972431222e
Souvik Dutta <116168757+SouvikDutta19@users.noreply.github.com>
2026-03-14 20:35:03 +0530

    Create day198_bfs_graph_traversal.java

---
+++ day198_bfs_graph_traversal.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class day198_bfs_graph_traversal {
+
+    static void bfs(List<List<Integer>> adj, int start) {
+        boolean[] visited = new boolean[adj.size()];
+        Queue<Integer> queue = new LinkedList<>();
+
+        visited[start] = true;
+        queue.add(start);
+
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            System.out.print(node + " ");
+
+            for (int neighbor : adj.get(node)) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    queue.add(neighbor);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int V = 5;
+        List<List<Integer>> adj = new ArrayList<>();
+
+        for (int i = 0; i < V; i++)
+            adj.add(new ArrayList<>());
+
+        adj.get(0).add(1);
+        adj.get(0).add(2);
+        adj.get(1).add(3);
+        adj.get(2).add(4);
+
+        bfs(adj, 0);
+    }
+}
