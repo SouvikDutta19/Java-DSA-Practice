30e8caf12c898699838c5c54df4788e075c746c0
Souvik Dutta <116168757+SouvikDutta19@users.noreply.github.com>
2026-05-05 23:24:20 +0530

    Create day210_sliding_window_maximum.java

---
+++ day210_sliding_window_maximum.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class day210_sliding_window_maximum {
+
+    public static int[] maxSlidingWindow(int[] nums, int k){
+
+        Deque<Integer> dq = new ArrayDeque<>();
+        int[] result = new int[nums.length - k + 1];
+
+        for(int i=0;i<nums.length;i++){
+
+            while(!dq.isEmpty() && dq.peekFirst() <= i-k)
+                dq.pollFirst();
+
+            while(!dq.isEmpty() && nums[dq.peekLast()] < nums[i])
+                dq.pollLast();
+
+            dq.offerLast(i);
+
+            if(i >= k-1)
+                result[i-k+1] = nums[dq.peekFirst()];
+        }
+
+        return result;
+    }
+}
