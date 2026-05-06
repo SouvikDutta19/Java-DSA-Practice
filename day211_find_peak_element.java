b8ed65987a5edece019053680b06edb206165987
Souvik Dutta <116168757+SouvikDutta19@users.noreply.github.com>
2026-05-06 22:23:50 +0530

    Create day211_find_peak_element.java

---
+++ day211_find_peak_element.java
@@ -0,0 +1,19 @@
+public class day211_find_peak_element {
+
+    public static int findPeakElement(int[] nums){
+
+        int left = 0, right = nums.length-1;
+
+        while(left < right){
+
+            int mid = (left + right) / 2;
+
+            if(nums[mid] > nums[mid+1])
+                right = mid;
+            else
+                left = mid + 1;
+        }
+
+        return left;
+    }
+}
