ff03ed0ef6fcb698a86e2afd5368923ee322ce94
Souvik Dutta <116168757+SouvikDutta19@users.noreply.github.com>
2026-06-21 23:51:46 +0530

    Create day215_number_of_islands.java

---
+++ day215_number_of_islands.java
@@ -0,0 +1,40 @@
+public class day215_number_of_islands {
+
+    public static int numIslands(char[][] grid) {
+
+        int count = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+
+            for (int j = 0; j < grid[0].length; j++) {
+
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    private static void dfs(char[][] grid,
+                            int row,
+                            int col) {
+
+        if (row < 0 ||
+            col < 0 ||
+            row >= grid.length ||
+            col >= grid[0].length ||
+            grid[row][col] == '0') {
+            return;
+        }
+
+        grid[row][col] = '0';
+
+        dfs(grid, row + 1, col);
+        dfs(grid, row - 1, col);
+        dfs(grid, row, col + 1);
+        dfs(grid, row, col - 1);
+    }
+}
