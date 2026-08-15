8a9a75c3071d6ec5012aef38347befd5d3b51e5a
Souvik Dutta <116168757+SouvikDutta19@users.noreply.github.com>
2026-08-15 20:38:09 +0530

    Create day216_longest_increasing_subsequence.java

---
+++ day216_longest_increasing_subsequence.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class day216_longest_increasing_subsequence {
+
+    public static int lengthOfLIS(int[] nums) {
+
+        if (nums.length == 0)
+            return 0;
+
+        int[] tails =
+                new int[nums.length];
+
+        int size = 0;
+
+        for (int num : nums) {
+
+            int left = 0;
+            int right = size;
+
+            while (left < right) {
+
+                int mid =
+                        left + (right - left) / 2;
+
+                if (tails[mid] < num)
+                    left = mid + 1;
+                else
+                    right = mid;
+            }
+
+            tails[left] = num;
+
+            if (left == size)
+                size++;
+        }
+
+        return size;
+    }
+}
